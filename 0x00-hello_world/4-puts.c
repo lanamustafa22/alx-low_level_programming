4c1791163d70494d0e42a41ad62955303ea865a3
lana mustafa <lanamustafa20000@gmail.com>
2022-12-08 00:12:34 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
+
+
 /**
- * main - Entry point
  *
- * Description: prints "Programming is like building a multilingual puzzle"
+ *  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ *   *
+ *
+ *    * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+
 int main(void)
+
 {
-	puts("("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
