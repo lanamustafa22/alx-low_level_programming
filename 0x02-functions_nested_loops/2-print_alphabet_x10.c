4cc9cbe3dc48a01cefc39d9e0b97b8f513c76b29
lana mustafa <lanamustafa20000@gmail.com>
2022-12-13 04:17:35 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print alphabet 10 times
+*
+*Description: print alphabet 10 times
+*Return void
+*
+*/
+void print_alphabet_x10(void)
+{
+int time = 0;
+char letter = 'a';
+while (time <= 10)
+{
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+time++;
+}
+}
