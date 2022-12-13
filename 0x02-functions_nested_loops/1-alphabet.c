ceee507d4c730cb5bb8a518788cee21bef967fe7
lana mustafa <lanamustafa20000@gmail.com>
2022-12-13 03:41:10 -0800

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_alphabet - print alphabet
+*
+*Return void
+*
+*/
+void print_alphabet(void)
+{
+int o;
+for (i = 'a'; o <= 'z' ; i++)
+{
+_putchar(o);
+}
+_putchar('\n');
+}
+
