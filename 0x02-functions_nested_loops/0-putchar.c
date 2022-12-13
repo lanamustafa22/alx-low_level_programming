3c6b1aad4cf9a7c59b13d544bb87e4c6eba8a278
lana mustafa <lanamustafa20000@gmail.com>
2022-12-13 03:23:36 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,14 @@
 */
 int main(void)
 {
-char word[10] = "_putchar"
-int i = 0;
-for (i = 0; i < 9; i++)
-{
-_putchar(word[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -8,5 +8,6 @@
 *on error, -1 is returned
 */
 int _putchar(char c)
+{
 return (write(1, &c ,1));
 }
