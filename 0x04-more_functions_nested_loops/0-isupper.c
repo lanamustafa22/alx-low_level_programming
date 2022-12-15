10e95eebe15bcd073f841e07634b4a789d010b73
lana mustafa <lanamustafa20000@gmail.com>
2022-12-15 01:21:33 -0800

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isupper - checks for uppercase character
+*@c:char
+*
+*Description:function that checks for uppercase character.
+*Return: 1,0
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+}
+else
+{
+return (0);
+}
+}
