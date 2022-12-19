2f1021e10bc6871ff51d7b5ef90195b46632cfcc
lana mustafa <lanamustafa20000@gmail.com>
2022-12-19 04:54:42 -0800

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*_strcpy - Copies a string pointed to by @src
+*@dest: A buffer to copy the string to
+*@src: The source string to copy
+*Return: A pointer to the destination string @dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+int len, i;
+
+len = 0;
+
+while (src[len] != '\0')
+{
+len++;
+}
+
+for (i = 0; i < len; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+
+return (dest);
+}
