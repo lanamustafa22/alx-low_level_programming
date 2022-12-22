11bbf36e5e305f86b21ca0cd280bfa147f97d972
lana mustafa <lanamustafa20000@gmail.com>
2022-12-22 00:47:21 -0800

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_strncat -  function that concatenates two strings
+*@dest: destnation
+*@src: source
+*@n: number of bytes from src
+*Return: new string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int len = 0;
+while (dest[len] != '\0')
+{
+len++;
+}
+for (i = 0 ; i < n && src[i] != '\0' ; i++)
+dest[len + i] = src[i];
+dest[len + i] = '\0';
+return (dest);
+}
