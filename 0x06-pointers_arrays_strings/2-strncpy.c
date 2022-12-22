6335055647fd77dbbbea9a371f5e65f04cf83aef
lana mustafa <lanamustafa20000@gmail.com>
2022-12-22 00:59:53 -0800

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_strncpy - function that copies a string
+*@dest: destnation
+*@src: sourse
+*@n: new string
+*Return: new string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return (dest);
+}
