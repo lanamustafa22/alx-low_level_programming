f1d857bcc8576065a25a07692bbf38822b40cce2
lana mustafa <lanamustafa20000@gmail.com>
2022-12-22 01:43:44 -0800

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,18 +2,22 @@
 
 /**
 *_strcmp - function that compares two strings
-*@*s1: first string
-*@*s2: second string
-*Return: 0, positive , negative
-*
+*@s1: first string
+*@s2: second string
+*Return: less than 0 if s1 is less than s2, 0 if they're equal,
+*more than 0 if s1 is greater than s2
 */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && *s2 && *s1 == *s2)
+while (*s1 == *s2)
 {
-*s1++;
-*s2++;
+if (*s1 == '\0')
+{
+return (0);
 }
-return (*s1 - *s2)
+s1++;
+s2++;
+}
+return (*s1 - *s2);
 }
 
